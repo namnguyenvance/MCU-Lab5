254b055af40bf3b57a7bbd668a2d2708a72f95ed
ZEPHYRUS\nvnha <nam.nguyenvance@hcmut.edu.vn>
2023-12-21 23:17:13 +0700

    Init Lab 5

---
+++ STM32 Code/Core/Inc/global.h
@@ -0,0 +1,18 @@
+/*
+ * global.h
+ *
+ *  Created on: Dec 21, 2023
+ *      Author: nvnha
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include <command_parser.h>
+#include "main.h"
+#include "software_timer.h"
+#include "uart.h"
+
+extern char str[];
+
+#endif /* INC_GLOBAL_H_ */
